c9d5c862fb46421415dda7ff8569e098b39d3cf7
ChilbyMadandan <93659195+ChilbyMadandan@users.noreply.github.com>
2021-11-04 03:43:33 +0900

    Update controler.java

--- controler.java
+++ controler.java
@@ -16,11 +16,7 @@ import java.util.stream.Collectors;
 public class Controller implements Initializable {
 
     ArrayList<String> words = new ArrayList<>(
-            Arrays.asList("test", "dog","Human", "Days of our life", "The best day",
-                    "Friends", "Animal", "Human", "Humans", "Bear", "Life",
-                    "This is some text", "Words", "222", "Bird", "Dog", "A few words",
-                    "Subscribe!", "SoftwareEngineeringStudent", "You got this!!",
-                    "Super Human", "Super", "Like")
+            Arrays.asList()
     );
 
     @FXML
